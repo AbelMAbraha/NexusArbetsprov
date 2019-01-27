7e0ac3337b353bf1f09e1cc7293b46c9ad08b076
AbelM <AbelM@LAPTOP-IO1L74LN>
2019-01-27 02:42:13 +0100

    Submit Project

---
+++ NexusArbetsprov/NexusArbetsprov/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NexusArbetsprov.MvcApplication" Language="C#" %>
---
+++ NexusArbetsprov/NexusArbetsprov/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NexusArbetsprov.Startup))]
+namespace NexusArbetsprov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
